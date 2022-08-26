04b7f8bee3c44054caa8ffd07491c16d6a88723a
Mohammed <mhmdtajeldin@gmail.com>
2022-08-26 04:32:20 +0200

    code for passing props between components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+    ];
+  
+  const handleChange = event => {
+    event.preventDefault();
+    console.log(event);
+    // console.log(event.target.value);
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <hr />
+      <List list={stories}/>
     </div>
   );
-}
+};
+
+const List = (props) => {
+  //  props.list.map(item => {
+  //   return (
+  //     <div key={item.objectID}>
+  //       <span>
+  //       <a href={item.url}>{item.title}</a>
+  //       </span>
+  //       <span>{item.author}</span>
+  //       <span>{item.num_comments}</span>
+  //       <span>{item.points}</span>
+  //     </div>
+  //     );
+  //   });
+  return props.list.map(item => {
+    return (
+      <div key={item.objectID}>
+        <span>
+        <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+      );
+    });
+};
 
 export default App;
